dd6ed1b946b1686e32c973304b8b34f0a1739f66
Fatih Karaoğlu <fatih39333@gmail.com>
2026-02-15 18:46:20 +0300

    feat: switch from static usleep to exponential pooling This commit aims to lower the initialize duration. Tested-by: Fatih Karaoğlu <fatih39333@gmail.com>
    Signed-off-by: Fatih Karaoğlu <fatih39333@gmail.com>

--- src/init.c
+++ src/init.c
@@ -5,9 +5,10 @@
 #include "hw.h"
 #include "ixgbe.h"
 /*
- * Writing registers following 82599 datasheet page 166.
+ * Initialize ASIC following 82599 datasheet page 166.
  */
 int ixgbe_probe(const struct hw* hw) {
+  u32 delay = 10;
   /* Disable Interrupts */
   ixgbe_write_reg(hw, IXGBE_EIMC, 0x7FFFFFFF);
   u32 err = ixgbe_read_reg(hw, IXGBE_EIMS);
@@ -16,22 +17,32 @@ int ixgbe_probe(const struct hw* hw) {
   u32 ctrl = ixgbe_read_reg(hw, IXGBE_CTRL);
   ctrl |= (IXGBE_CTRL_RST | IXGBE_CTRL_LRST);
   ixgbe_write_reg(hw, IXGBE_CTRL, ctrl);
-  usleep(10000);
-  err = ixgbe_read_reg(hw, IXGBE_CTRL);
+  /* ~10.23 ms at total. +~10ms means malfunctional behavior. */
+  delay = 10;
+  for (u8 i = 0; i < 15; i++) {
+    err = ixgbe_read_reg(hw, IXGBE_CTRL);
+    if (likely(!(err & (IXGBE_CTRL_RST | IXGBE_CTRL_LRST)))) break;
+    usleep(delay);
+    if (likely(delay < 1000)) delay *= 2;
+  }
   if (unlikely(err & (IXGBE_CTRL_RST | IXGBE_CTRL_LRST))) return -ETIMEDOUT;
-  u8 i;
-  for (i = 0; i < 50; i++) {
+  /* Reset delay value since it'll continue from Global Reset. */
+  delay = 10;
+  for (u8 i = 0; i < 23; i++) {
     const u32 eeprom = ixgbe_read_reg(hw, IXGBE_EEC);
     if (eeprom & IXGBE_EEC_ARD) goto eeprom_ok;
-    usleep(1000);
+    usleep(delay);
+    if (likely(delay < 1000)) delay *= 2;
   }
   if (unlikely(i == 50)) return -ENODEV;
 eeprom_ok:
-  /* EEPROM read duration is 20ms at max */
-  for (i = 0; i < 50; i++) {
+  /* ~20.47 ms at total. +~20ms means malfunctional behavior. */
+  delay = 10;
+  for (u8 i = 0; i < 23; i++) {
     const u32 dmaidone = ixgbe_read_reg(hw, IXGBE_RDRXCTL);
     if (dmaidone & IXGBE_RDRXCTL_DMAIDONE) goto dmaiok;
-    usleep(1000);
+    usleep(delay);
+    if (likely(delay < 1000)) delay *= 2;
   }
   if (unlikely(i == 50)) return -ETIMEDOUT;
 dmaiok:;
